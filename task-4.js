a30ac3d1fa224a59e21ee1acb765670ade780199
Sanjayan <v.sanjayan200606@gmail.com>
2026-03-11 12:22:46 +0530

    Add files via upload

---
+++ task-4.js
@@ -0,0 +1,29 @@
+// Function Declaration:
+
+// function multiply(a,b){
+//     console.log(a*b)
+// }
+// multiply(4,5)
+
+// Function Expression:
+
+// let number = function (a){
+//     console.log(a*a)
+// }
+// number(2)
+
+// Arrow function:
+
+// const greet= (name)=>{
+//     console.log(name, "Vanakkam da maapla")
+// }
+// greet("sanjay")
+
+//string metode
+
+let msg="Java is a popular programming language used to build powerful applications. Many developers use Java to create web, mobile, and enterprise software";
+console.log(msg.trim().toLowerCase());
+console.log(msg.replaceAll("java", "Python").toUpperCase());
+console.log(msg.indexOf("Java"));
+console.log(msg.charAt(3));
+console.log(msg.includes("letter"))
